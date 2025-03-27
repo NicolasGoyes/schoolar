353d3b6193ba09804151b29ee7b69504e7e93b43
Nicolas <N@gmail.com>
2025-03-27 11:34:46 -0500

    post ARREGLADO

--- src/signup.php
+++ src/signup.php
@@ -1,7 +1,7 @@
 <?php
 
 //aqui se llama la conexion $conn el cual se encuentra en databasephp
-    include('../config/database.php') //se le pone .. porque esta fuera de crs  es como un cd
+    include('../config/database.php'); //se le pone .. porque esta fuera de crs  es como un cd
 
     $fname = $_POST['f_name']; //VARIABLE = METODO ['NOMBRES NAME DEL HTML']
     $lname = $_POST['l_name'];
@@ -9,9 +9,19 @@
     $passwd = $_POST['passw'];
     
     //datos de values van en comilla sencilla y tienen que ser los mismos de arriba 
-    $sql ="INSERT INTO users (firstname, lastname, email, pasword)
+    $sql ="INSERT INTO users (firstname, lastname, email, password)
         VALUES('$fname', '$lname', '$email', '$passwd') 
 
     ";
-51buº
+//para ejectuar va el $res=pg_query(); dentro del query van la coneccion
+//el pg query es como un f5 el cual corre el esq con $conn
+    $res = pg_query($conn, $sql);
+
+    if($res){
+        echo  "User has been created succesfully";
+    }
+    else {
+        echo  "Error";
+    }
+
 ?>
