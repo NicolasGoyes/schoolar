103bb03ae7a0b8b45d44267e61fad0b5e764c487
Nicolas <N@gmail.com>
2025-03-28 18:10:04 -0500

    validation

--- src/signup.php
+++ src/signup.php
@@ -7,21 +7,40 @@
     $lname = $_POST['l_name'];
     $email = $_POST['e_mail'];
     $passwd = $_POST['passw'];
-    
-    //datos de values van en comilla sencilla y tienen que ser los mismos de arriba 
-    $sql ="INSERT INTO users (firstname, lastname, email, password)
-        VALUES('$fname', '$lname', '$email', '$passwd') 
 
-    ";
+    $enc_pass = md5($passwd);
+
+    //creamos otro sql que nos permita validar si el email existe
+    $sql_email_exist = "SELECT COUNT (email) as total FROM users WHERE email ='$email' LIMIT 1";
+    $res = pg_query($conn, $sql_email_exist);
+
+    if ($res){
+        $row = pg_fetch_assoc($res);
+        if ($row['total'] > 0){
+            echo "Email already exist";
+        }
+        else {
+            $sql ="INSERT INTO users (firstname, lastname, email, password)
+        VALUES('$fname', '$lname', '$email', '$enc_pass')
+        ";
+
+            $res = pg_query($conn, $sql);
+
+            if($res){
+                echo  "User has been created succesfully";
+            }
+            else {
+                echo  "Error";
+            }
+        }
+    }
+
+
+
+    //datos de values van en comilla sencilla y tienen que ser los mismos de arriba 
+    
 //para ejectuar va el $res=pg_query(); dentro del query van la coneccion
 //el pg query es como un f5 el cual corre el esq con $conn
-    $res = pg_query($conn, $sql);
-
-    if($res){
-        echo  "User has been created succesfully";
-    }
-    else {
-        echo  "Error";
-    }
+   
 
 ?>
